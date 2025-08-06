637250471a8abe906b5f641d26ea843abeb251b7
Syedmujtaba2002 <syedmujtabahussaini03@gmail.com>
2025-08-07 01:12:31 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,5 +4,6 @@ def test_home():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert "Hello from Flask CI/CD App and welcome to Elevate labs 🚀" in response.data.decode('utf-8')
+    assert "Hello from Flask CI/CD App and welcome to Elevate labs 🚀
+            This is the webhook process" in response.data.decode('utf-8')
 
