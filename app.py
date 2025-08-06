1597e21f7cb47e2b663b571f022caf4abd6aec0a
Syedmujtaba2002 <syedmujtabahussaini03@gmail.com>
2025-08-06 23:14:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask CI/CD App and welcome to Elevate labs🚀"
+    return "Hello from Flask CI/CD App and welcome to Elevate labs 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
