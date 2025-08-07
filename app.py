2ce45ad304d9215949438baa723451a1e1124878
Syedmujtaba2002 <syedmujtabahussaini03@gmail.com>
2025-08-07 15:28:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask CI/CD App and welcome to Elevate labs 🚀This is the webhook process"
+    return "Hello from Flask CI/CD App and welcome to Elevate labs 🚀This is the webhook process and elevate labs"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
